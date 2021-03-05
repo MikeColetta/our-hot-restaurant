0786fd41a4beb900d2871ac5cb94d273e5fc3a38
Jesse Willig <jessewillig@outlook.com>
2021-03-04 21:00:46 -0800

    added module.exports

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,15 @@
-const path = require('path');
+const path = require("path");
 
-app.get('/table', (req, res) => {
-    res.sendFile(path.join(__dirname, '../tables.html'));
-});
+module.exports = (app) => {
+  app.get("/table", (req, res) => {
+    res.sendFile(path.join(__dirname, "../tables.html"));
+  });
 
-app.get('/reserve', (req, res) => {
-    res.sendFile(path.join(__dirname, '..reserve.html'));
-});
+  app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "..reserve.html"));
+  });
 
-app.get('/reserve', (req, res) => {
-    res.sendFile(path.join(__dirname, '..index.html'));
-});
+  app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "..index.html"));
+  });
+};
